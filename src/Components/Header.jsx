48223fd934687f3231836645dda40158bc5e5357
Rohit raj <126317686+rohit-raj7@users.noreply.github.com>
2024-12-09 17:54:31 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Navbar from './Navbar';
 import { LayoutGroup, motion } from "framer-motion";
@@ -10,49 +9,62 @@ function Header() {
   return (
     <div
       id="Header"
-      className="ml-4  min-h-screen flex flex-col md:flex-row justify-between items-center w-full pt-12 overflow-hidden relative"
+      className="ml-4 min-h-screen flex flex-col md:flex-row justify-between items-center w-full pt-12 overflow-hidden relative"
     >
       <Navbar />
 
+      {/* Left Section */}
       <motion.div
-        
-        
         className="flex flex-col items-start text-left px-12 py-8 lg:w-1/2"
       >
         <h2 className="text-4xl sm:text-5xl md:text-[80px] font-semibold">
           Hey there, It's <span className="animated-text">Rohit Raj</span>
         </h2>
-        <div className="space-x-6 mt-8">
-          <a href="#Project" className="border border-white px-8 py-3 rounded ">
-                     Projects
-          </a>
-          <a href="#Contact" className="bg-blue-500 px-8 py-3 rounded">
-            Contact Us
-          </a>
-        </div>
+       
+            <div className="space-x-6 mt-8">
+                 <a
+               href="#Project"
+               className="border border-white px-6 py-2 rounded text-sm sm:px-8 sm:py-3 sm:text-base"
+             >
+               Projects
+             </a>
+             <a
+               href="#Contact"
+               className="bg-blue-500 px-6 py-2 rounded text-sm sm:px-8 sm:py-3 sm:text-base"
+             >
+               Contact Us
+             </a>
+           </div>
+
         <div className="mt-8">
           <Social />
         </div>
       </motion.div>
 
+      {/* Right Section */}
       <motion.div
         initial={{ scale: 0.9, opacity: 0 }}
         whileInView={{ scale: 1, opacity: 1 }}
         viewport={{ once: false, amount: 0.5 }}
         transition={{ duration: 0.75, delay: 0.1 }}
-        className="relative flex-1 flex justify-center items-center mt-4"
+        className="relative flex-1 flex justify-center items-center mt-4 w-90%"
       >
-       <img
-  src={assets.main_img}
-  alt="Header"
-  className="w-13 h-13 object-cover max-w-[500px] max-h-[500px] rounded-md"
-/>
+        
+         <img
+          src={assets.rohit_img}
+          className="w-[63%] rounded-lg"
+          alt="Rohit"
+          />
+
 
       </motion.div>
-      <div className='invisible'> <Logo/></div>
+
+      {/* Hidden Logo */}
+      <div className="hidden">
+        <Logo />
+      </div>
     </div>
   );
 }
 
 export default Header;
-
