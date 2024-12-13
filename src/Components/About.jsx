888de3537a313f92799fbbaf0242a585839103ec
Rohit raj <126317686+rohit-raj7@users.noreply.github.com>
2024-12-13 20:40:15 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { motion } from 'framer-motion';
 import { assets } from '../assets/assets';
@@ -15,22 +16,23 @@ function About() {
           Our Brand
         </span>
       </h1>
-      
-<div className="flex flex-col items-center md:items-center mt-10 text-white">
-        <div className="grid grid-cols-2 gap-6 md:gap-10 w-full pr-28 border-b-2 border-gray-700 pb-8">
+
+      <div className="flex flex-col items-center md:items-center mt-10 text-white">
+        <div className="grid grid-cols-2 gap-6 md:gap-10 w-full md:pr-28 border-b-2 border-gray-700 pb-8">
           <div className="border-r-2 border-gray-600 pr-6">
-            <p className="text-4xl font-medium ">1+</p>
+            <p className="text-4xl font-medium">1+</p>
             <p className='text-gray-300'>Year of Experience</p>
           </div>
           <div className="border-r-2 border-gray-700 pr-6">
-            <a href="#Project"><p className="text-4xl font-medium">4+</p>
-            <p className='text-gray-300'>Projects Completed</p>
+            <a href="#Project">
+              <p className="text-4xl font-medium">4+</p>
+              <p className='text-gray-300'>Projects Completed</p>
             </a>
           </div>
           <div className="border-r-2 border-gray-700 pr-6">
             <a href="https://leetcode.com/u/rohit-raj7/">
-            <p className="text-4xl font-medium">300+</p>
-            <p className='text-gray-300'>Questions Solved on LeetCode</p>
+              <p className="text-4xl font-medium">300+</p>
+              <p className='text-gray-300'>Questions Solved on LeetCode</p>
             </a>
           </div>
           <div>
