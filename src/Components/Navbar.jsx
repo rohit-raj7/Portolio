4a26ad3280a9d4551d96ddf45cd53ad14b7e2004
Rohit raj <126317686+rohit-raj7@users.noreply.github.com>
2024-12-30 17:52:40 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -22,35 +22,12 @@ function Navbar() {
     <div className="absolute top-0 left-0 w-full z-10">
       <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
         <div className="flex items-center space-x-2">
-{/*           <img src={assets.logo} alt="Logo" className="h-10 w-10" /> */}
-           <div className="text-white flex items-center gap-1 text-2xl">
-      
-      <div className="text-gray-100 text-4xl">
-        <p>&#123;</p>
-      </div>
-      
-      <div className="flex flex-row">
-        <p>&#82;</p>
-        <p>&#79;</p>
-        <p>&#72;</p>
-        <p>&#73;</p>
-        <p>&#84;</p>
-      </div>
-      {/* Gap */}
-      <div className="w-0"></div>
-      
-      <div className="flex flex-row">
-        <p>&#82;</p>
-        <p>&#65;</p>
-        <p>&#74;</p>
-      </div>
-      {/* Closing Bracket */}
-      <div className="text-gray-100 text-4xl">
-        <p>&#125;</p>
-      </div>
-    </div>
+
           {/* <Rlogo/> */}
-          {/* <span className='text-3xl font-medium'>Rohit Raj</span> */}
+           <div className="flex items-center">
+            <Rlogo />
+        
+          </div>
         </div>
 
         <ul className="hidden md:flex gap-7 text-lg text-white">
@@ -119,6 +96,44 @@ function Navbar() {
 
 export default Navbar;
 
+
+const Rlogo = () => {
+  return (
+    <div className="flex justify-center items-center w-12 h-12 mr-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        viewBox="0 0 100 100"
+        width="100"
+        height="100"
+        aria-label="Hexagon with R"
+      >
+        {/* Hexagon */}
+        <polygon
+          points="50,5 93,27.5 93,72.5 50,95 7,72.5 7,27.5"
+          fill="none"
+          stroke="#00FFFF"
+          strokeWidth="6"
+            className="rounding-border"
+        />
+        {/* Letter R */}
+        <text
+          x="50%"
+          y="50%"
+          textAnchor="middle"
+          // fill="#00FFFF"
+          fill="#00FFFF"
+          fontSize="45px"
+          fontFamily="Consolas, monospace"
+          dominantBaseline="middle"
+          fontWeight="bold"
+        >
+          R
+        </text>
+      </svg>
+    </div>
+  );
+};
 
 
 
