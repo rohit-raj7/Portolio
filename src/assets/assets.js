e3118029251cbe9407266c25d8f495d335b76958
Rohit raj <126317686+rohit-raj7@users.noreply.github.com>
2025-01-31 09:16:59 +0530

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,69 +1,66 @@
-import logo from './logo.png'
-import logo_dark from './logo.png'
-import cross_icon from './cross_icon.svg'
-import menu_icon from './menu_icon.svg'
-import star_icon from './star_icon.svg'
-import left_arrow from './left_arrow.svg'
-import right_arrow from './right_arrow.svg'
-import header_img from './header_img.png'
-import brand_img from './brand_img.png'
-import education_img from './education_img.gif'
-import weather_img from './weather_logo.jpg'
-import queue_img from './Queue_img.jpg'
-import Task_management_img from './Task_management_img.png'
-import Auto_fill_img from './222028.png'
-import PortfolioLogo from './PortfolioLogo.jpg'
-import rohit_img from './rohit.png'
+import logo from './logo.png';
+import logo_dark from './logo.png';
+import cross_icon from './cross_icon.svg';
+import menu_icon from './menu_icon.svg';
+import star_icon from './star_icon.svg';
+import left_arrow from './left_arrow.svg';
+import right_arrow from './right_arrow.svg';
+import header_img from './header_img.png';
+import brand_img from './brand_img.png';
+import education_img from './education_img.gif';
+import weather_img from './weather_logo.jpg';
+import queue_img from './Queue_img.jpg';
+import Task_management_img from './Task_management_img.png';
+import Auto_fill_img from './222028.png';
+import PortfolioLogo from './PortfolioLogo.jpg';
+import rohit_img from './rohit.png';
 import bacckground_img from './200w.webp';
-import playButton_img from './play-icon.png'
-import message_img from './message.png'
-import main_img from './main_img.gif'
-import code_img2 from './coding_img2.png'
-import code_img3 from './coding_img3.png'
-import code_img4 from './coding_img4.png'
-import code_img5 from './coding_img5.png'
-import code_img6 from './code_img6.png'
-import code_img7 from './coding_img7.png'
-import language_img1 from './c_logo.png'
-import language_img2 from './java_logo.png'
-import language_img3 from './java_script_logo.png'
-import language_img4 from './react_logo.png'
-import language_img5 from './tailwid_logo.png'
-import language_img6 from './css_logo.png'
-import language_img7 from './code_img8.png'
-import language_img8 from './code_img9.png'
-import language_img9 from './netlify.png'
-import language_img10 from './vite_js.png'
-import language_img11 from './mysql-img.png'
-import language_img12 from './cpp_logo.png'
-import github_img1 from './github_logo.png'
-import link_img from './link.png'
-import api_img from './apis_img.png'
-import react_img from './react.webp'
-import github_img from './github.webp'
-import monogo_db from './mongo-db.webp'
-import tailwind_img from './tailwind.webp'
-import node_img from './nodejs.webp'
-import git_img from './git_logo.png'
-import gec_banka_img from './gec_banka_img.png'
-import inter_college_img from './Sarvajanik Inter College Samukhiamore, Banka.png'
-import pwjava_img from './pwjavaDsa.png'
-import selceforce_cerificat_img from './selceforce_cerificat_img.png'
-import Cisco_cerificat_img from './Cisco_img.jpeg'
-import CodeIndex_certificate_img from './codindex.png'
-import x_img from './x.png'
-import salesforce from './salesforce-cover.png'
-import learningx from './learningx.png'
-import links from './links.png'
+import playButton_img from './play-icon.png';
+import message_img from './message.png';
+import main_img from './main_img.gif';
+import code_img2 from './coding_img2.png';
+import code_img3 from './coding_img3.png';
+import code_img4 from './coding_img4.png';
+import code_img5 from './coding_img5.png';
+import code_img6 from './code_img6.png';
+import code_img7 from './coding_img7.png';
+import language_img1 from './c_logo.png';
+import language_img2 from './java_logo.png';
+import language_img3 from './java_script_logo.png';
+import language_img4 from './react_logo.png';
+import language_img5 from './tailwid_logo.png';
+import language_img6 from './css_logo.png';
+import language_img7 from './code_img8.png';
+import language_img8 from './code_img9.png';
+import language_img9 from './netlify.png';
+import language_img10 from './vite_js.png';
+import language_img11 from './mysql-img.png';
+import language_img12 from './cpp_logo.png';
+import github_img1 from './github_logo.png';
+import link_img from './link.png';
+import api_img from './apis_img.png';
+import react_img from './react.webp';
+import github_img from './github.webp';
+import monogo_db from './mongo-db.webp';
+import tailwind_img from './tailwind.webp';
+import node_img from './nodejs.webp';
+import git_img from './git_logo.png';
+import gec_banka_img from './gec_banka_img.png';
+import inter_college_img from './Sarvajanik Inter College Samukhiamore, Banka.png';
+import pwjava_img from './pwjavaDsa.png';
+import selceforce_cerificat_img from './selceforce_cerificat_img.png';
+import Cisco_cerificat_img from './Cisco_img.jpeg';
+import CodeIndex_certificate_img from './codindex.png';
+import x_img from './x.png';
+import salesforce from './salesforce-cover.png';
+import learningx from './learningx.png';
+import links from './links.png';
 
 export const assets = {
     logo,
     x_img,
     salesforce,
     api_img,
-    pwjava_img,
-    linked_certificate_img,
-    Cisco_cerificat_img,
     pwjava_img,
     selceforce_cerificat_img,
     link_img,
@@ -88,7 +85,8 @@ export const assets = {
     code_img6,
     code_img7,
     react_img,
-    github_img,monogo_db,
+    github_img,
+    monogo_db,
     tailwind_img,
     inter_college_img,
     git_img,
@@ -105,90 +103,38 @@ export const assets = {
     learningx,
     links,
     CodeIndex_certificate_img
-   
-
-}
-
+};
 
 export const skillData = [
   {
     category: "programming",
     skills: [
-      {
-        logo: language_img2,
-        title: "Java",
-      },
-      {
-        logo: language_img12,
-        title: "C++",
-      },
-      {
-        logo: language_img3,
-        title: "JavaScript",
-      },
-      {
-        logo: language_img6,
-        title: "CSS",
-      },
-      {
-        logo: code_img6,
-        title: "HTML",
-      },
-      {
-        logo: code_img7,
-        title: "Python",
-      },
+      { logo: language_img2, title: "Java" },
+      { logo: language_img12, title: "C++" },
+      { logo: language_img3, title: "JavaScript" },
+      { logo: language_img6, title: "CSS" },
+      { logo: code_img6, title: "HTML" },
+      { logo: code_img7, title: "Python" },
     ],
   },
   {
     category: "frameworks",
     skills: [
-      {
-        logo: language_img4,
-        title: "React",
-      },
-      {
-        logo: language_img5,
-        title: "Tailwind CSS",
-      },
-      {
-        logo: language_img7,
-        title: "Bootstrap",
-      },
+      { logo: language_img4, title: "React" },
+      { logo: language_img5, title: "Tailwind CSS" },
+      { logo: language_img7, title: "Bootstrap" },
     ],
   },
-
   {
     category: "tools",
     skills: [
-      {
-        logo: git_img,
-        title: "Git",
-      },
-      {
-        logo: github_img1,
-        title: "GitHub",
-      },
-      {
-        logo: language_img8,
-        title: "Gitlab",
-      },
-      {
-        logo: language_img9,
-        title: "Netlify",
-      },
-      {
-        logo: language_img10,
-        title: "ViteJS",
-      },
-      {
-        logo: language_img11,
-        title: "Mysql",
-      },
-      {
-        logo: api_img,
-        title: "Api",
-      },
+      { logo: git_img, title: "Git" },
+      { logo: github_img1, title: "GitHub" },
+      { logo: language_img8, title: "Gitlab" },
+      { logo: language_img9, title: "Netlify" },
+      { logo: language_img10, title: "ViteJS" },
+      { logo: language_img11, title: "Mysql" },
+      { logo: api_img, title: "Api" },
     ],
   },
 ];
@@ -198,94 +144,85 @@ export const ExperienceData = [
     year: "Dec 2023 - Jun 2024",
     title: "Salesforce",
     position: "Virtual Internship",
-    description:
-      "The Salesforce virtual internship offers hands-on experience in CRM development, Trailhead learning, and Superbadges, enhancing technical and problem-solving skills.",
-    img:salesforce,
-    link:links,
-    link_src:"https://smartinternz.com/internships/salesforce_certificates/9084c6f938aedee18e63af5c2167ecab" ,
+    description: "The Salesforce virtual internship offers hands-on experience in CRM development, Trailhead learning, and Superbadges, enhancing technical and problem-solving skills.",
+    img: salesforce,
+    link: links,
+    link_src: "https://smartinternz.com/internships/salesforce_certificates/9084c6f938aedee18e63af5c2167ecab",
   },
   {
     year: "Oct 2024 - Dec 2024",
     title: "LearningX",
     position: "Frontend Developer Internship",
-    description:
-      "As a frontend intern, I worked with JavaScript and React to build interactive websites. I collaborated with APIs to fetch, manipulate, and display data, optimizing the user experience.",
+    description: "As a frontend intern, I worked with JavaScript and React to build interactive websites. I collaborated with APIs to fetch, manipulate, and display data, optimizing the user experience.",
     img: learningx,
-    link:links,
-    link_src:"www.yout.coe" ,
+    link: links,
+    link_src: "www.yout.coe",
   },
   {
     year: "2025",
     title: "Upcoming",
     position: "XYZ",
-    description:
-      "Developed scalable web solutions for millions of users worldwide. Developed scalable web solutions for millions of users worldwide. Developed scalable web solutions for millions of users worldwide.",
-    img:salesforce,
-    link:links,
-    link_src:"www.yout.coe" ,
+    description: "Developed scalable web solutions for millions of users worldwide.",
+    img: salesforce,
+    link: links,
+    link_src: "www.yout.coe",
   },
 ];
 
-
-
 export const projectsData = [  
-    {
-      title: "JAVA + DSA",
-      links_img:link_img,
-      src: "https://pwskills.com/learn/certificate/0ccd5dac-79ec-40ef-9f7c-0b92f71976a5/",
-      image: pwjava_img
-    },
-    {
-      title: "Generative AI by Microsoft",
-      links_img: link_img,
-      src: "https://www.linkedin.com/learning/certificates/11d141cd8da7828ef4a3b66d19c2b8b95d4f5026a55529ab2830a995991632d2?trk=share_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3Bzic%2FK%2F%2F5RYCLmoxOYXprJg%3D%3D",
-      image:linked_certificate_img
-    },
-   
-    {
-      title: "Salesforce Developer Intern",
-      links_img: link_img,
-      src: "https://smartinternz.com/internships/salesforce_certificates/9084c6f938aedee18e63af5c2167ecab",
-      image:selceforce_cerificat_img
-    },
-    {
-      title: "Cisco Certificate",
-      links_img: link_img,
-      src: "https://drive.google.com/file/d/163sipqfZJRw7ji-12igCcun_ME-888Iv/view",
-      image:Cisco_cerificat_img
-    },
-   {
-      title: "CODEXINTERN virtual internship",   
-      link: "Chicago",
-      links_img: link_img,
-      src: "https://drive.google.com/file/d/1eHpe-wX2eNs8V_JNDQdXtK2SRvF82FC_/view?usp=sharing",
-      image: CodeIndex_certificate_img
-    },
-    
-  ];
-
+  {
+    title: "JAVA + DSA",
+    links_img: link_img,
+    src: "https://pwskills.com/learn/certificate/0ccd5dac-79ec-40ef-9f7c-0b92f71976a5/",
+    image: pwjava_img
+  },
+  {
+    title: "Generative AI by Microsoft",
+    links_img: link_img,
+    src: "https://www.linkedin.com/learning/certificates/11d141cd8da7828ef4a3b66d19c2b8b95d4f5026a55529ab2830a995991632d2?trk=share_certificate&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3Bzic%2FK%2F%2F5RYCLmoxOYXprJg%3D%3D",
+    image: linked_certificate_img
+  },
+  {
+    title: "Salesforce Developer Intern",
+    links_img: link_img,
+    src: "https://smartinternz.com/internships/salesforce_certificates/9084c6f938aedee18e63af5c2167ecab",
+    image: selceforce_cerificat_img
+  },
+  {
+    title: "Cisco Certificate",
+    links_img: link_img,
+    src: "https://drive.google.com/file/d/163sipqfZJRw7ji-12igCcun_ME-888Iv/view",
+    image: Cisco_cerificat_img
+  },
+  {
+    title: "CODEXINTERN virtual internship",   
+    links_img: link_img,
+    src: "https://drive.google.com/file/d/1eHpe-wX2eNs8V_JNDQdXtK2SRvF82FC_/view?usp=sharing",
+    image: CodeIndex_certificate_img
+  },
+];
 
 export const projectData = [
   {
-    name:"Autofiller",
+    name: "Autofiller",
     title: "Tech Stack",
-    techStack: [code_img6, language_img6,language_img3],
+    techStack: [code_img6, language_img6, language_img3],
     image: Auto_fill_img,
     github: github_img,
-    github_src:"https://github.com/rohit-raj7/Fill-Form-Auto",
+    github_src: "https://github.com/rohit-raj7/Fill-Form-Auto",
     link: link_img,
-    link_src:"https://github.com/rohit-raj7/Fill-Form-Auto",
+    link_src: "https://github.com/rohit-raj7/Fill-Form-Auto",
     text: "Automatically fill forms with one click, saving time and improving efficiency and accuracy...",
   },
   {
     name: "N-Queens Visualizer",
     title: "Tech Stack",
-    techStack: [code_img6, language_img6,language_img3],
+    techStack: [code_img6, language_img6, language_img3],
     image: queue_img,
     github: github_img,
-    github_src:"https://github.com/rohit-raj7/queens-visulation",
+    github_src: "https://github.com/rohit-raj7/queens-visulation",
     link: link_img,
-    link_src:"https://queue-visulation.netlify.app/",
+    link_src: "https://queue-visulation.netlify.app/",
     text: "The N-Queens Visualizer demonstrates step-by-step solutions to the N-Queens problem with clear visual representation...",
   },
   {
@@ -294,31 +231,31 @@ export const projectData = [
     techStack: [language_img4, language_img5, language_img6],
     image: Task_management_img,
     github: github_img,
-    github_src:"https://github.com/rohit-raj7/Task-Management-Dashboard",
+    github_src: "https://github.com/rohit-raj7/Task-Management-Dashboard",
     link: link_img,
-    link_src:"https://rohit-raj7.github.io/task-anagement/",
+    link_src: "https://rohit-raj7.github.io/task-anagement/",
     text: "The Task Management project helps users organize tasks efficiently...",
   },
   {
     name: "Weather App",
     title: "Tech Stack",
-    techStack: [code_img6,language_img6,language_img3,api_img],
+    techStack: [code_img6, language_img6, language_img3, api_img],
     image: weather_img,
     github: github_img,
-    github_src:"https://github.com/rohit-raj7",
+    github_src: "https://github.com/rohit-raj7",
     link: link_img,
-    link_src:"https://weathe-app-rohit.netlify.app/",
+    link_src: "https://weathe-app-rohit.netlify.app/",
     text: "A weather app that provides real-time data using a weather API...",
   },
-    {
+  {
     name: "Portfolio",
     title: "Tech Stack",
     techStack: [language_img4, language_img5, language_img6],
     image: PortfolioLogo,
     github: github_img,
-    github_src:"https://github.com/rohit-raj7/Portolio",
+    github_src: "https://github.com/rohit-raj7/Portolio",
     link: link_img,
-    link_src:"https://rohit-raj.netlify.app/",
+    link_src: "https://rohit-raj.netlify.app/",
     text: "Create a personal portfolio using React: showcase projects, skills, responsive design, and interactive user interface...",
   },
 ];
