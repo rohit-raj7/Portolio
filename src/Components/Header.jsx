4347aa6808415cff70733eee4f636e0413add2c6
Rohit raj <126317686+rohit-raj7@users.noreply.github.com>
2025-04-08 22:56:10 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,11 @@
-
 import React from 'react';
 import Navbar from './Navbar';
 import { LayoutGroup, motion } from "framer-motion";
 import Social from './Social';
 import { assets } from "../assets/assets";
-import Logo from './Logo';
-import './header.css'
+ 
+import './header.css' 
+import { LiveUserCount } from '../UserCount/LiveUserCount';
 
 function Header() {
   return (
@@ -21,6 +21,8 @@ function Header() {
         <h2 className="text-4xl sm:text-5xl md:text-[80px] font-semibold">
           Hey there, It's <span className="animated-text">Rohit Raj</span>
         </h2>
+        <div className="flex flex-col lg:flex-row">
+         
 
         <div className="space-x-6 mt-8">
           <a
@@ -32,11 +34,19 @@ function Header() {
           <a
             href="#Contact"
             className="bg-blue-500 px-6 py-2 rounded text-sm sm:px-8 sm:py-3 sm:text-base"
+              // className="bg-pink-500 px-6 py-2 rounded text-sm sm:px-8 sm:py-3 sm:text-base"
           >
             Contact Us
           </a>
-        </div>
 
+          </div>
+        
+  <div className='gap-1 sm:ml-6'>
+    <LiveUserCount />
+  </div> 
+          </div> 
+         
+        
         <div className="mt-8">
           <Social />
         </div>
@@ -51,11 +61,13 @@ function Header() {
       >
         <div className="rohit-container">
           <img src={assets.rohit_img} alt="Profile" />
+        
         </div>
+
       </motion.div>
 
       <div className="hidden">
-{/*         <Logo /> */}
+        {/* <Logo /> */}
       </div>
     </div>
   );
@@ -69,15 +81,13 @@ export default Header;
 
 
 
-
-
-
 // import React from 'react';
 // import Navbar from './Navbar';
 // import { LayoutGroup, motion } from "framer-motion";
 // import Social from './Social';
 // import { assets } from "../assets/assets";
 // import Logo from './Logo';
+// import './header.css'
 
 // function Header() {
 //   return (
@@ -87,58 +97,130 @@ export default Header;
 //     >
 //       <Navbar />
 
-//       {/* Left Section */}
 //       <motion.div
 //         className="flex flex-col items-start text-left px-12 py-8 lg:w-1/2"
 //       >
 //         <h2 className="text-4xl sm:text-5xl md:text-[80px] font-semibold">
 //           Hey there, It's <span className="animated-text">Rohit Raj</span>
 //         </h2>
-       
-//             <div className="space-x-6 mt-8">
-//                  <a
-//                href="#Project"
-//                className="border border-white px-6 py-2 rounded text-sm sm:px-8 sm:py-3 sm:text-base"
-//              >
-//                Projects
-//              </a>
-//              <a
-//                href="#Contact"
-//                className="bg-blue-500 px-6 py-2 rounded text-sm sm:px-8 sm:py-3 sm:text-base"
-//              >
-//                Contact Us
-//              </a>
-//            </div>
+
+//         <div className="space-x-6 mt-8">
+//           <a
+//             href="#Project"
+//             className="border border-white px-6 py-2 rounded text-sm sm:px-8 sm:py-3 sm:text-base"
+//           >
+//             Projects
+//           </a>
+//           <a
+//             href="#Contact"
+//             className="bg-blue-500 px-6 py-2 rounded text-sm sm:px-8 sm:py-3 sm:text-base"
+//           >
+//             Contact Us
+//           </a>
+//         </div>
 
 //         <div className="mt-8">
 //           <Social />
 //         </div>
 //       </motion.div>
 
-//       {/* Right Section */}
 //       <motion.div
 //         initial={{ scale: 0.9, opacity: 0 }}
 //         whileInView={{ scale: 1, opacity: 1 }}
 //         viewport={{ once: false, amount: 0.5 }}
 //         transition={{ duration: 0.75, delay: 0.1 }}
-//         className="relative flex-1 flex justify-center items-center mt-4 w-90%"
+//         className="relative flex-1 flex justify-center items-center mt-4 w-[90%]"
 //       >
-        
-//          <img
-//           src={assets.rohit_img}
-//           className="w-[63%] rounded-lg"
-//           alt="Rohit"
-//           />
-
-
+//         <div className="rohit-container">
+//           <img src={assets.rohit_img} alt="Profile" />
+//         </div>
 //       </motion.div>
 
-//       {/* Hidden Logo */}
 //       <div className="hidden">
-//         <Logo />
+// {/*         <Logo /> */}
 //       </div>
 //     </div>
 //   );
 // }
 
 // export default Header;
+
+
+
+
+
+
+
+
+
+
+// // import React from 'react';
+// // import Navbar from './Navbar';
+// // import { LayoutGroup, motion } from "framer-motion";
+// // import Social from './Social';
+// // import { assets } from "../assets/assets";
+// // import Logo from './Logo';
+
+// // function Header() {
+// //   return (
+// //     <div
+// //       id="Header"
+// //       className="ml-2 min-h-screen flex flex-col md:flex-row justify-between items-center w-full pt-12 overflow-hidden relative"
+// //     >
+// //       <Navbar />
+
+// //       {/* Left Section */}
+// //       <motion.div
+// //         className="flex flex-col items-start text-left px-12 py-8 lg:w-1/2"
+// //       >
+// //         <h2 className="text-4xl sm:text-5xl md:text-[80px] font-semibold">
+// //           Hey there, It's <span className="animated-text">Rohit Raj</span>
+// //         </h2>
+       
+// //             <div className="space-x-6 mt-8">
+// //                  <a
+// //                href="#Project"
+// //                className="border border-white px-6 py-2 rounded text-sm sm:px-8 sm:py-3 sm:text-base"
+// //              >
+// //                Projects
+// //              </a>
+// //              <a
+// //                href="#Contact"
+// //                className="bg-blue-500 px-6 py-2 rounded text-sm sm:px-8 sm:py-3 sm:text-base"
+// //              >
+// //                Contact Us
+// //              </a>
+// //            </div>
+
+// //         <div className="mt-8">
+// //           <Social />
+// //         </div>
+// //       </motion.div>
+
+// //       {/* Right Section */}
+// //       <motion.div
+// //         initial={{ scale: 0.9, opacity: 0 }}
+// //         whileInView={{ scale: 1, opacity: 1 }}
+// //         viewport={{ once: false, amount: 0.5 }}
+// //         transition={{ duration: 0.75, delay: 0.1 }}
+// //         className="relative flex-1 flex justify-center items-center mt-4 w-90%"
+// //       >
+        
+// //          <img
+// //           src={assets.rohit_img}
+// //           className="w-[63%] rounded-lg"
+// //           alt="Rohit"
+// //           />
+
+
+// //       </motion.div>
+
+// //       {/* Hidden Logo */}
+// //       <div className="hidden">
+// //         <Logo />
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
+// // export default Header;
