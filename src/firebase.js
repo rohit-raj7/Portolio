f7422ad392e229023a1a409e0109cb8fcf26c624
Rohit raj <126317686+rohit-raj7@users.noreply.github.com>
2025-04-09 09:47:03 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,21 @@
-
-
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue, set, update, increment } from "firebase/database";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set,
+  remove,
+  get,
+  child,
+  increment,
+  update
+} from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAozd4rZK6V-F2XpItCIUGkxJ8djlf0cCo",
   authDomain: "portfolio-30f88.firebaseapp.com",
   projectId: "portfolio-30f88",
-  storageBucket: "portfolio-30f88.firebasestorage.app",
+  storageBucket: "portfolio-30f88.appspot.com",
   messagingSenderId: "854272581929",
   appId: "1:854272581929:web:d12e0077f7708bae139738",
   measurementId: "G-5BLB3C2GGL"
@@ -16,5 +24,14 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-export { database, ref, onValue, set, update, increment };
- 
+export {
+  database,
+  ref,
+  onValue,
+  set,
+  remove,
+  get,
+  child,
+  increment,
+  update
+};
